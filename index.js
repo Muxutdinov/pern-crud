d02e7fd4f93f11dfec0d91a0ae8c612ec3ccbc75
Muxutdinov <jmuxutdinov@mail.ru>
2022-06-07 18:17:09 +0500

    second commit

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
-//connect to mongoDB
-mongoose.connect(
-  "mongodb+srv://Jaxongir:mern-register@cluster0.zqk0jwy.mongodb.net/register",
-  () => {
-    console.log("Connected to MONGODB");
-  }
-);
+const user = require("./models/user");
 
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ msg: "Hello from Backend development" });
+//connect to mongoDB
+mongoose.connect(
+  "mongodb+srv://Jaxongir:mern-register@cluster0.zqk0jwy.mongodb.net/register"
+);
+
+// use JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//METHOD POST
+app.post("/signup", (req, res) => {
+  const { name, email, password } = req.body;
+  if ((!name || !email, !password)) {
+    res.status(400).json({ msg: "Iltimos barcha inputlarni to'ldiring" });
+  }
+
+  res.status(200).json({ message: "signup mofaqiyatli amalga ishirildi!" });
 });
 
+//LISTEN SERVER
 app.listen(5000, () => {
   console.log("server run on port 5000");
 });
-// mern-register
+
+//MONGODB PASSWORD =  mern-register
